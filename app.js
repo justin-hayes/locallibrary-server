ef5620b741e4afb848157500df736c5fde032da9
Justin Hayes <jhayes84@gmail.com>
2017-06-05 23:42:27 -0400

    Completed part 3 of the tutorial.

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://db'
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error."'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
